b4a555a83065d08a597dca202b55861448a68ceb
Rozana-IM <rozanaim09@gmail.com>
2026-01-30 17:01:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
-const API_BASE_URL = "http://localhost:8080"; // API Gateway / ALB URL later
+const API_BASE_URL = "http://localhost:8080";
 
 async function loadUsers() {
   const res = await fetch(`${API_BASE_URL}/users`);
   const data = await res.json();
-  document.getElementById("result").innerText =
-    JSON.stringify(data, null, 2);
+
+  const target =
+    document.getElementById("usersResult") ||
+    document.getElementById("result");
+
+  target.innerText = JSON.stringify(data, null, 2);
 }
 
 async function loadOrders() {
   const res = await fetch(`${API_BASE_URL}/orders`);
   const data = await res.json();
-  document.getElementById("result").innerText =
-    JSON.stringify(data, null, 2);
-}
 
+  const target =
+    document.getElementById("ordersResult") ||
+    document.getElementById("result");
+
+  target.innerText = JSON.stringify(data, null, 2);
+}
