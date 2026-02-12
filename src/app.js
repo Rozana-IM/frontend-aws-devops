9044209601d8af4259746228d11f1b6da72e95ed
Rozana-IM <rozanaim09@gmail.com>
2026-02-12 19:34:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://rozana-projects.online;
 
 async function loadUsers() {
   const res = await fetch(`${API_BASE_URL}/users`);
