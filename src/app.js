bf48ae3612c36878210b7210229615aaacb970a6
Rozana-IM <rozanaim09@gmail.com>
2026-04-07 22:50:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -294,3 +294,16 @@ window.addEventListener("load", async () => {
   }
 
 });
+
+/*  TOAST MESSAGE */
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  if (!toast) return;
+
+  toast.innerText = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+}
